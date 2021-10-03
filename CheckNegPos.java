0d169b8464811b1771e6f67c87e8c688ff2025cf
ManiSekar91 <91821306+ManiSekar91@users.noreply.github.com>
2021-10-03 13:58:14 +0530

    Add files via upload

---
+++ CheckNegPos.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class CheckNegPos {
+
+	public static void main(String[] args) {
+int a = -35;
+if (a>0) {
+	System.out.println("The value is Positive");
+	
+} else if(a<0){ 
+
+	System.out.println("The value is Negative");
+}
+else 
+{
+	System.out.println("The value is Neither Postive nor Negative");
+}
+	}
+
+}
---
+++ NegativePositive.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class NegativePositive {
+
+	public static void main(String[] args) {
+int a = -35;
+if (a<0) {
+	System.out.println("The value is Negative" +a);
+}
+int b = a* -1;
+System.out.println("The Converted Value is " +b);
+
+	}
+}
+
